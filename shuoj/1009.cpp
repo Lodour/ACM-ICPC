ee4480f5fa0752e5766676bf19a5b0ca7833ba5a
mangogao <jjmangogao@gmail.com>
2015-09-21 00:01:01 +0800

    build
    codes of shuoj

---
+++ shuoj/1009.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+    return 0;
+}
+
+/**************************************************************
+    Problem: 1009
+    User: 14123090
+    Language: C++
+    Result: Accepted
+    Time:0 ms
+    Memory:1560 kb
+****************************************************************/
---
+++ shuoj/README.md
@@ -0,0 +1,2 @@
+# SHUOJ
+Codes of my solved problems in [SHUOJ](http://202.121.199.212/JudgeOnline/)
